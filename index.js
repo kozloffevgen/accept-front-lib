c8d5e599487b0d390102adbe39ef12310ccff145
EVGEN <inevitability.k@gmail.com>
2022-04-24 10:14:17 +0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const commander = require('commander')
+const { builder } = require('./webpack/config.build')
 
-commander.command('a').action(() => {
-  console.log('123');
+commander.command('build').action(() => {
+  console.log(builder);
+})
+
+commander.command('start').action(() => {
+  console.log('start');
 })
 
 commander.parse(process.argv);
