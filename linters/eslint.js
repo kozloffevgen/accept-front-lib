41af0bd3204aa30e8e01ee27073521f1b4b8f243
EVGEN <inevitability.k@gmail.com>
2022-05-28 13:32:50 +0300

    update eslint

--- linters/eslint.js
+++ linters/eslint.js
@@ -1,6 +1,40 @@
 module.exports = {
-  "parserOptions": {
-      "sourceType": "module",
-      "ecmaVersion": 2020
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 2
+      },      
+      "multiline": {
+        "max": 1
+      }
+    }],
+    "vue/block-tag-newline": ["error", {
+      "singleline": "always",
+      "multiline": "always",
+      "maxEmptyLines": 1
+    }],
+    "vue/component-name-in-template-casing": ["error", "PascalCase", {
+      "registeredComponentsOnly": true,
+    }],
+    "vue/html-button-has-type": ["error", {
+      "button": true,
+      "submit": true,
+      "reset": true
+    }],
+    "vue/new-line-between-multi-line-property": ["error", {
+      "minLineOfMultilineProperty": 2
+    }],
+    "vue/no-potential-component-option-typo": ["error", {
+      "presets": ["all"],
+      "custom": ["test"]
+    }],
+    "vue/no-undef-components": ["error", {
+      "ignorePatterns": []
+    }],
+    "vue/padding-line-between-blocks": ["error", "always"]
   }
 }
