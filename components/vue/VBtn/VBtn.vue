e3e316944f9aaae184d6a9b88cefddf8ce78f6c2
EVGEN <inevitability.k@gmail.com>
2022-05-30 17:13:44 +0300

    add VBtn component

---
+++ components/vue/VBtn/VBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="v-btn" :type="btnType">
+    {{ btnText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VBtn',
+  props: {
+    btnType: {String, default: 'button'},
+    btnText: '',
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import './styles/v-btn.less';
+</style>
